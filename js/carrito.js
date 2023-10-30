6b372fd3cdb8a1d7d9b9b29263c038c4cf41f5e2
valentina <jimenez.valentina2002@gmail.com>
2023-10-30 02:11:34 -0300

    misProductos

--- js/carrito.js
+++ js/carrito.js
@@ -29,7 +29,7 @@ function traerProductosLS() {
 // }
 
 async function traerProductos() {
-    const response = await fetch('../js/productos.json');
+    const response = await fetch('../js/misProductos.json');
     if (response.ok) {
         producto = await response.json();
         elejirProducto();
